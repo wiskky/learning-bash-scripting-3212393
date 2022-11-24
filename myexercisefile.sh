c085f2339b090aabb3423268cae46c9dc4bca662
Wisdom <adeniyi.wisdom@gmail.com>
2022-11-24 15:03:51 +0000

    Updating myexefile

---
+++ myexercisefile.sh
@@ -0,0 +1,31 @@
+#Bash variable
+#mygreetings=Hello,
+#mygreetings2=" Good morning!"
+#echo ${mygreetings}
+#echo ${mygreetings2}
+#echo -n ${mygreetings}${mygreetings2}
+
+# myvar="Hello"
+# echo "This is my "$myvar
+
+# declare -r myname="Wisdom"
+# echo "My name is $myname"
+
+# declare -l tolower="THIS IS MY NAME"
+# echo "This text change to lower letter $tolower"
+
+# declare -u toupper="this is my name"
+# echo "This text change to lower letter $toupper"
+
+# a="adeniyi"
+# echo $a
+
+# a="Wisdom"
+# echo $a
+
+#comapre with
+declare -r  a="adeniyi"
+echo $a
+
+a="Wisdom"
+echo $a
